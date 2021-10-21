e4779a01504cb001a569fedee86e0e974a4e680e
Lilian Virginia Lombardi López <virginia.lombardi@globant.com>
2021-10-21 01:23:26 -0300

    finish app
    finish app

---
+++ IAmRichAPP/IAmRichAppViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  IAmRichAPP
+//
+//  Created by Virginia Lombardi Lopez on 20/10/21.
+//
+
+import UIKit
+
+class IAmRichAppViewController: UIViewController {
+//    MARK: - VIEWDIDLOAD FUNC
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
